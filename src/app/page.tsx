d0c3ffbef5d03a24bf2e951650685287578753a0
francis <clyderitchie@yahoo.com>
2024-08-21 19:50:46 -0700

    Major wireframe skelton done for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import NavBar from '@/app/ui/navbar'
 import SideBar from './ui/sideBar';
 import PostLayout from './ui/postsLayout';
+import Highlights from './ui/highlights';
 
 export default function Home() {
   return (
@@ -12,6 +13,9 @@ export default function Home() {
         </div>
         <div className='min-h-screen max-h-screen min-w-screen-md max-w-screen-lg mt-5 ml-5 border flex flex-col items-center justify-center'>
           <PostLayout />
+        </div>
+        <div className='min-h-screen max-h-screen min-w-48 max-w-56 mt-5 ml-5 border flex flex-col items-center justify-center'>
+          <Highlights/>
         </div>
       </div>
     </main>
---
+++ src/app/ui/highlights.tsx
@@ -0,0 +1,7 @@
+export default function Highlights() {
+    return (
+        <div>
+            Post 1
+        </div>
+    )
+}
